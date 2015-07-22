1a514710d558d0dbc105a63080ad2ebd8e73c338
stevey_ttt@hotmail.co.uk <stevey_ttt@hotmail.co.uk>
2015-07-22 20:03:21 +1000

    first commit

---
+++ sudo/manifests/init.pp
@@ -0,0 +1,21 @@
+##First Sudo module
+class sudo {
+
+  package { 'sudo':
+    ensure => present,
+}
+if $::osfamily == 'Debian' {
+  package { 'sudo-ldap':
+    ensure  => present,
+    require => Package['sudo'],
+  }
+}
+
+  file { '/etc/sudoers':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0440',
+    source  => "puppet://${::server}/modules/sudo/etc/sudoers",
+    require => Package['sudo'],
+  }
+}
